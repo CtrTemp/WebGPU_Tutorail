d5ca10f252a6afbcdc05cfe6fe795f2aaf0bc47c
CtrTemp <1744909116@qq.com>
2023-10-22 17:07:03 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <Suspense>
+    <WebGPURoot/>
+  </Suspense>
+    <!-- <WebGPURoot/> -->
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import WebGPURoot from './components/WebGPURoot.vue';
+
+
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+// }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
