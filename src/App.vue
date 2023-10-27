c301b275c8aaf3fef5ddea2792b604916468e109
CtrTemp <1744909116@qq.com>
2023-10-27 20:57:09 +0800

    ts version

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,30 @@
-<template>
-  <Suspense>
-    <!-- <WebGPURoot/> -->
-    <!-- <Tuto02_RenderGrid/> -->
-    <!-- <Tuto03_RenderState/> -->
-    <!-- <Tuto04_ConwayLifeGame/> -->
-    <!-- <Tuto05_Triangle/> -->
-    <!-- <Tuto06_MSAA/> -->
-    <!-- <Tuto07_CanvasResize/> -->
-    <!-- <Tuto08_RotatingCube/> -->
-    <!-- <Tuto09_twoCubes/> -->
-    <!-- <Tuto10_TexturedCube/> -->
-    <!-- <Tuto11_InstancedCube/> -->
-    <!-- <Tuto12_FractalCube/> -->
-    <Tuto13_Camera/>
-  </Suspense>
-    <!-- <WebGPURoot/> -->
-</template>
-
-<script setup>
-import WebGPURoot from './components/WebGPURoot.vue';
-import Tuto02_RenderGrid from './components/Tuto02_RenderGrid.vue';
-import Tuto03_RenderState from './components/Tuto03_RenderState.vue';
-import Tuto04_ConwayLifeGame from './components/Tuto04_ConwayLifeGame.vue';
-import Tuto05_Triangle from './components/Tuto05_Triangle.vue';
-import Tuto06_MSAA from './components/Tuto06_MSAA.vue';
-import Tuto07_CanvasResize from './components/Tuto07_CanvasResize.vue';
-import Tuto08_RotatingCube from './components/Tuto08_RotatingCube.vue';
-import Tuto09_twoCubes from './components/Tuto09_twoCubes.vue';
-import Tuto10_TexturedCube from './components/Tuto10_TexturedCube.vue';
-import Tuto11_InstancedCube from './components/Tuto11_InstancedCube.vue';
-import Tuto12_FractalCube from './components/Tuto12_FractalCube.vue';
-import Tuto13_Camera from './components/Tuto13_Camera.vue';
-
-
-// export default {
-//   name: 'App',
-//   components: {
-//     HelloWorld
-//   }
-// }
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
