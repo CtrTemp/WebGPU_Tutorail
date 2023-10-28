f883c8c297019a2093614e861789fee01f1563d1
CtrTemp <1744909116@qq.com>
2023-10-28 14:26:36 +0800

    TypeScript Demo Cubes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import DemoCube from './components/00_Demo_Cube.vue'
+import DemoInstancedCube from './components/01_Demo_InstancesCube.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Suspense>
+    <DemoCube/>
+    <!-- <DemoInstancedCube/> -->
+  </Suspense>
 </template>
 
 <style scoped>
